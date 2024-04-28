81208bbbb2a3cb8299656f17a8dabfa9441948a8
Moussa Dembele <134782079+Md123etplus@users.noreply.github.com>
2024-04-28 17:05:27 +0100

    Add files via upload

---
+++ process/process_login.php
@@ -0,0 +1,27 @@
+<?php
+include('../includes/connection.inc.php');
+include('../includes/fn.inc.php');
+
+if (isset($_POST['send'])) {
+    session_start();
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $user = login_user($conn, $email, $password);
+    if (!empty($user)) {
+        $_SESSION['userID'] = $user['id'];
+        $_SESSION['email'] = $user['mail'];
+        $_SESSION['user'] = $user['role'];
+        $_SESSION['image'] = $user['image'];
+        $_SESSION['name'] = $user['nom'];
+        if ($user['role'] == 'professeur') {
+            header('Location: ../home_professeur.php');
+            exit;
+        } else {
+            header('Location: ../home_etudiant.php');
+            exit;
+        }
+    } else {
+        echo 'Email ou mot de passe incorrect !';
+    }
+}
+?>
